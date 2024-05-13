288038a14c9da52f562a90afff5e8525d928c92f
sauloorizaga <159079803+sauloorizaga@users.noreply.github.com>
2024-05-13 16:31:17 -0600

    Add files via upload

--- BertoziMethod_Stability.m
+++ BertoziMethod_Stability.m
@@ -4,18 +4,15 @@
 function [Stability]=BertoziMethod_Stability(dt,M1,iter,tfinal)
 
 clear Energy time Mvar GUvar
-% tic;
 clc;
 
 Stability=1;
 
-M=20;
-M=6;
-a=-4*pi*0;
+M=12;
+a=0;
 b=M*pi;
-% for proper spacing   [0 2pi]
 %number of grid points N
-N=256*1/4;
+N=256;
 %uniform mesh thickness
 h=(b-a)/N;
 %(Periodic bdy conditions)
@@ -35,27 +32,14 @@ k=[[0:N/2] [-N/2+1:-1]]./((M+4*0)/2);
  k4=k2.^2;
 
 %Initial Condition---------------------------------
-% load('ICactive.mat','U');
-% %--------------------------------------------------
+load('ICactive.mat','U');
+%--------------------------------------------------
 
-U=0.5+0.1*exp(-((X-3).^2/20+Y.^2/20));
-U=0.1+0.9*exp(-(X.^2/2+Y.^2/2));   %figure 258
-
-U=0.1+0.9*exp(-((X-pi).^2/.75+(Y-pi).^2/.75));   %2pi domain
-
-U=0.1+1*exp(-5*((X-pi).^2+(Y-pi).^2));   %2pi domain
-
-U=0.8-.4*exp(-1*((X-pi).^2+(Y-pi).^2));   %2pi domain
-
-U=.6-.5*exp(-0.5*((X-5*pi).^2+(Y-5*pi).^2))-.5*exp(-.5*((X-2*pi).^2+(Y-2*pi).^2))-.5*exp(-1*((X-3*pi).^2+(Y-4*pi).^2));   %2pi domain
-
-U=.6-.5*exp(-.5*((X-2*pi).^2+1*(Y-1.5*pi).^2))-.5*exp(-.5*((X-4*pi).^2+1*(Y-1.5*pi).^2))+ ...
-   -.5*exp(-.5*((X-2*pi).^2+1*(Y-4*pi).^2))-.5*exp(-.5*((X-4*pi).^2+1*(Y-4*pi).^2))+...
-   -.5*exp(-.5*((X-3*pi).^2+1*(Y-4.5*pi).^2));
-
-
-% % figure(1);
-% % mesh(X,Y,U)
+figure(1);
+mesh(X,Y,U)
+ax = gca; 
+ax.FontSize = 14;
+colormap('jet')
 
 %parameters
 epsilon=.1*1;
@@ -64,7 +48,7 @@ eps2=epsilon^2;
 M1;
 Bertozzinumber=max(max(U.^3));
 Mvar(1)=max(max(U.^3));
-GUvar(1)= max(max(-eps*eps*(3.0.*log(U)+4.0*eps./U)+0.25.*(0.1).*U.^4));
+GUvar(1)= max(max(-epsilon*epsilon*(3.0.*log(U)+4.0*epsilon./U)+0.25.*(0.1).*U.^4));
 
 %The LHS, left hand side of problem----------
 lhs=1+dt*M1*k4;      %Tom W.
@@ -111,68 +95,82 @@ time(it+1)=t;
 
 Mvar(it+1)=max(max(U.^3));
 GUvar(it+1)= max(max(-eps*eps*(3.0.*log(U)+4.0*eps./U)+0.25.*(0.1).*U.^4));
-GUvar(it+1)= max(max(-eps*eps*(3.0.*log(U)+4.0*eps./U)+0.25*(0.1).*U.^4));
+GUvar(it+1)= max(max(-epsilon*epsilon*(3.0.*log(U)+4.0*epsilon./U)+0.25*(0.1).*U.^4));
 
 % % figure(3);                  %if monitoring the solution is needed.
 % % mesh(X,Y,U)
-
-if U<0
-    Stability=0;
-    break                   % unstable of U values are neg.
-end
 
 if Energy(it+1)>Energy(it)
     Stability=0;
     break                   % This break will stop the code if energy increases - unstable
 end
-
-% % if M1>0.14
-% %     Stability=1;
-% %     break                   % This break will stop the code if energy increases - unstable
-% % end
    
+% 
+% if M1>.3 && dt<20/2^6
+%     Stability=1;
+%     break                   % This break code for already known stanle regions
+% end
+% 
+% if M1>.28 && dt<20/2^12
+%     Stability=1;
+%     break                   % This break code for already known stanle regions
+% end
+% 
+% if M1<.22 && dt>20/2^14
+%     Stability=0;
+%     break                   % This break code for already known stanle regions
+% end
+
 
 end  %main loop
 
-% % figure(3);
-% % mesh(X,Y,U)
+figure(30);
+mesh(X,Y,U)
+ax = gca; 
+ax.FontSize = 14;
+colormap('jet')
+%title(['t_{F}=' num2str(t)] )
 
-% a=toc;
-% minutes=a/60;
-% hours=a/60^2;
-% minutes_hours=[minutes hours]
 
   U1=U;
+  load('Uexact','U');
+  error=h*h.*sum(sum(abs(U1-U)));          %L1 error
+ 
+ figure(31);
+mesh(X,Y,U)
+ax = gca; 
+ax.FontSize = 14;
+colormap('jet')
+%title(['t_{F}=' num2str(t)] )
 
-
- %Energy Plotting and Reporting Stability
+ %Energy Plotting and Reporting Stability 
+ % To be commented out for efficiency
 % % figure(5)
-% % plot(time,Energy,'o-')
+% % plot(time,Energy,'.-')
 % % axis([time(1) time(end) min(Energy) max(Energy)])
 % % xlabel('time')
 % % ylabel('Free Energy')
 % % 
 % % figure(6)
-% % plot(time,Mvar,'o-',time,GUvar,'o-')
-% % axis([time(1) time(end) min(Mvar)*0 max(Mvar)])
+% % %plot(time,Mvar,'o-',time,GUvar,'o-')
+% % plot(time,Mvar,'.-')
+% % axis([time(1) time(end) min(Mvar) max(Mvar)])
 % % h = legend('$\bar{M}(t)$');
 % % set(h,'interpreter','Latex','FontSize',12)
 % % %legend('$\bar M(t)$')
 % % xlabel('time')
-% % ylabel('M_1')
+% % %ylabel('M_1')
+% % h=ylabel('$\bar{M}(t)$');
+% % set(h,'interpreter','Latex','FontSize',12)
 % % 
 % % figure(7)
-% % plot(time,GUvar,'o-')
+% % plot(time,GUvar,'.-')
 % % axis([time(1) time(end) min(GUvar) max(GUvar)])
 % % h = legend('$\bar{G}(t)$');
 % % set(h,'interpreter','Latex','FontSize',12)
 % % %legend('$\bar M(t)$')
 % % xlabel('time')
-% % ylabel('G_1')
-
-
-
-
-
-
+% % %ylabel('G_1')
+% % h=ylabel('$\bar{G}(t)$');
+% % set(h,'interpreter','Latex','FontSize',12)
 end
